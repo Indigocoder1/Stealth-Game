19eaa1ea7a902a145921ad61e0038eebc13372f4
INSANENITRO5\sayhi <isaakthompson27@gmail.com>
2023-11-10 14:59:44 -0800

    This is basic firing mechanics with a placeholder taser. The movement of the gun needs to be linked with the turning mechanics, but I didn't want to touch Caleb's code.

---
+++ Stealth Game/Assets/Scripts/FiringScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class FiringScript : MonoBehaviour
+{
+    public Transform bullet;
+    public Transform bulletHolder;
+    public float bulletSpeed = 10;
+    public float fireRate = 2;
+    private float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && time >= fireRate)
+        {
+            time = 0;
+            Transform bulletTrans = Instantiate(bullet, bulletHolder.position, bullet.rotation);
+            Rigidbody bulletRB = bulletTrans.GetComponent<Rigidbody>();
+            bulletRB.AddForce(Vector3.forward * bulletSpeed * Time.deltaTime * 100f);
+        }
+        time += Time.deltaTime;
+    }
+}
