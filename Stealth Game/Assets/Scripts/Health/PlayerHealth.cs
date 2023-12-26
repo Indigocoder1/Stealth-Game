bb88416ea3936caf1098209e8daaa15b9e953e2d
Indigocoder1 <caleb@liston.us>
2023-12-26 10:15:31 -0800

    Added health system w/ health bar

---
+++ Stealth Game/Assets/Scripts/Health/PlayerHealth.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    [SerializeField] private int health = 100;
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private Image healthColor;
+    [SerializeField] Gradient healthColorGradient;
+
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = health;
+
+        healthBar.maxValue = 1;
+        healthBar.minValue = 0;
+
+        float normalizedHealth = (float)currentHealth / health;
+        healthColor.color = healthColorGradient.Evaluate(normalizedHealth);
+        healthBar.value = normalizedHealth;
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Damage(10);
+        }
+#endif
+    }
+
+    public void Damage(int damage)
+    {
+        currentHealth -= damage;
+        float normalizedHealth = (float)currentHealth / health;
+
+        healthColor.color = healthColorGradient.Evaluate(normalizedHealth);
+        healthBar.value = normalizedHealth;
+    }
+
+    public int GetHealth()
+    {
+        return currentHealth;
+    }
+
+    public float GetHealthNormalized()
+    {
+        return (float)currentHealth / health;
+    }
+}
