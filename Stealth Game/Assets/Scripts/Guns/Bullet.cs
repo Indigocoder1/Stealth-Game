562c5644c45b448b23d058a5170a2db060c1d101
Indigocoder1 <caleb@liston.us>
2023-12-26 10:20:39 -0800

    Added damaging to bullets

--- Stealth Game/Assets/Scripts/Guns/Bullet.cs
+++ Stealth Game/Assets/Scripts/Guns/Bullet.cs
@@ -3,19 +3,30 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private Transform owner;
+    private int damage;
 
     public void SetOwner(Transform owner)
     {
         this.owner = owner;
     }
 
+    public void SetDamage(int damage)
+    {
+        this.damage = damage;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.TryGetComponent(out IDamageable damageable))
         {
-            //decrease health and stuff
+            DamageTarget(damageable);
         }
 
         Destroy(gameObject);
+    }
+
+    protected virtual void DamageTarget(IDamageable damageable)
+    {
+        damageable.Damage(damage);
     }
 }
